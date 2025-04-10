5a675593b68bfba7778ac9a66048a8ceb419dd10
Satyam Kumar <satyamk.1107@gmail.com>
2025-04-10 10:44:30 +0530

    Created components for Home and Seller-Auth

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-auth',
+  imports: [],
+  templateUrl: './seller-auth.component.html',
+  styleUrl: './seller-auth.component.css'
+})
+export class SellerAuthComponent {
+
+}

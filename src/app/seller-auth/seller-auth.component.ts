a87bdec44eb398569d4a9666d778a5156a48dcbe
Satyam Kumar <satyamk.1107@gmail.com>
2025-04-10 12:44:43 +0530

    Added signup form for Seller

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-auth',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './seller-auth.component.html',
   styleUrl: './seller-auth.component.css'
 })
 export class SellerAuthComponent {
-
+  signUp(data: NgForm): void {
+    console.warn(data.value)
+  }
 }

4e15f9cb8f5eb71dbe02eb6423078b666deaeea3
Satyam Kumar <satyamk.1107@gmail.com>
2025-04-11 11:14:25 +0530

    Created POST request for seller-auth component

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { SellerService } from '../services/seller.service';
 
 @Component({
   selector: 'app-seller-auth',
@@ -8,7 +9,12 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './seller-auth.component.css'
 })
 export class SellerAuthComponent {
+  constructor(private seller:SellerService) {}
+
   signUp(data: NgForm): void {
     console.warn(data.value)
+    this.seller.userSignUp(data.value).subscribe((result) => {
+      console.warn(result)
+    })
   }
 }

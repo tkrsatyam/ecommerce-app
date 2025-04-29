733550b91b98ad997d71953206423e64f97e8f5a
Satyam <satyamk.1107@gmail.com>
2025-04-29 11:45:32 +0530

    Updated header nav-bar for User login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,6 +11,7 @@ import { NavigationEnd, Router, RouterModule } from '@angular/router';
 export class HeaderComponent implements OnInit {
   menuType: string = 'default'
   sellerName: string = ''
+  userName: string = ''
 
   constructor(private readonly route: Router) { }
 
@@ -19,9 +20,14 @@ export class HeaderComponent implements OnInit {
       if (event instanceof NavigationEnd) {
         if (typeof window !== 'undefined' && localStorage.getItem('seller') && event.url.includes('seller')) {
           let seller = localStorage.getItem('seller');
-          let sellerData =seller && JSON.parse(seller);
+          let sellerData = seller && JSON.parse(seller);
           this.sellerName = sellerData[0].name;
           this.menuType = 'seller'
+        } else if (typeof window !== 'undefined' && localStorage.getItem('user')) {
+          let user = localStorage.getItem('user');
+          let userData = user && JSON.parse(user);
+          this.userName = userData[0].name;
+          this.menuType = 'user'
         } else {
           this.menuType = 'default'
         }
@@ -29,8 +35,13 @@ export class HeaderComponent implements OnInit {
     })
   }
 
-  logout() {
+  logoutSeller() {
     localStorage.removeItem('seller');
     this.route.navigate(['/'])
+  }
+
+  logoutUser() {
+    localStorage.removeItem('user');
+    this.menuType = 'default'
   }
 }

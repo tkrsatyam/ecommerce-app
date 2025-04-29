f09928593e35e0b19e5d6ef70d443f9e05f0575d
Satyam <satyamk.1107@gmail.com>
2025-04-29 16:21:02 +0530

    Added Form validation to Seller and User auth pages

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -2,16 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { SellerService } from '../services/seller.service';
 import { CommonModule } from '@angular/common';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-seller-auth',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, FontAwesomeModule],
   templateUrl: './seller-auth.component.html',
   styleUrl: './seller-auth.component.css'
 })
 export class SellerAuthComponent implements OnInit {
   showSignUp = false
   authError: string = '';
+  showPassword: boolean = false;
+  showLoginPassword: boolean = false;
+  eye = faEye;
+  eyeSlash = faEyeSlash;
+
   constructor(private readonly seller: SellerService) { }
 
   ngOnInit(): void {
@@ -25,7 +32,7 @@ export class SellerAuthComponent implements OnInit {
   login(data: NgForm): void {
     this.seller.sellerLogin(data.value)
     this.seller.isLoginError.subscribe((isError) => {
-      if(isError) {
+      if (isError) {
         this.authError = 'Email or password is not correct. Please try again.'
       }
     })
--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -3,16 +3,23 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { login, signUp } from '../data-type';
 import { UserService } from '../services/user.service';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-user-auth',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, FontAwesomeModule],
   templateUrl: './user-auth.component.html',
   styleUrl: './user-auth.component.css'
 })
 export class UserAuthComponent implements OnInit {
   showSignUp = false
   authError: string = '';
+  showPassword: boolean = false;
+  showLoginPassword: boolean = false;
+  eye = faEye;
+  eyeSlash = faEyeSlash;
+
   constructor(private readonly user: UserService) { }
 
   ngOnInit(): void {
